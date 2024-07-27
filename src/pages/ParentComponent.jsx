44885a9153eed7ab11fb81f2247176c111642f79
robiul99siyam <siyamrobiulislam@gmail.com>
2024-07-27 11:10:17 +0600

    video content open problem solve

--- src/pages/ParentComponent.jsx
+++ src/pages/ParentComponent.jsx
@@ -5,10 +5,10 @@ import Sidebar from './Sidebar';
 import Details from './Details';
 import Comment from './Comment';
 
-
 const ParentComponent = () => {
     const { id } = useParams();
     const [data, setData] = useState([]);
+    const [selectedId, setSelectedId] = useState(id);
 
     useEffect(() => {
         fetch("https://netfiex.onrender.com/netfiex/app/content/")
@@ -17,17 +17,20 @@ const ParentComponent = () => {
             .catch(err => console.error(err));
     }, []);
 
+    useEffect(() => {
+        setSelectedId(id);
+    }, [id]);
+
     return (
         <>
             <Navbar />
             <div className='flex gap-5 flex-col md:flex-row'>
-                <Details data={data} id={id} />
-                <Sidebar data={data} />
+                <Details data={data} id={selectedId} />
+                <Sidebar data={data} id={id} onItemSelect={setSelectedId} />
             </div>
             <div>
                 <Comment />
             </div>
-          
         </>
     );
 };
--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -1,34 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Sidebar = ({ data }) => {
-
-    // const [slider,setSlider]=useState(null)
-    // useEffect(() => {
-    //     const foundItem = data.find(item => String(item.id) === id);
-    //     setSlider(foundItem);
-    // }, [data, id]);
-
+const Sidebar = ({ data, onItemSelect, id }) => {
     return (
-        <>
-
-           
-
-
-            <div className="w-[30%] shadow-xl overflow-y-scroll h-[450px]">
-                {data.map(item => (
-                    <Link to={`/view-content/${item.id}/`} key={item.id} className="flex gap-3 font-medium hover:bg-gray-100 rounded-md p-2">
-
-
+        <div className="w-[30%] shadow-xl overflow-y-scroll h-[450px]">
+            {data.map(item => (
+                <Link to={`/view-content/${item.id}`} key={item.id}>
+                    <div
+                       
+                        className="flex gap-3 font-medium hover:bg-gray-100 rounded-md p-2 cursor-pointer"
+                        onClick={() => onItemSelect(item.id)}
+                    >
                         <img className="w-28 rounded-xl" src={item.thumbell} alt="" />
                         <div className="flex flex-col justify-center">
                             <p>{item.title}</p>
                             <p>Robiul Islam</p>
                         </div>
-                    </Link>
-                ))}
-            </div>
-        </>
+                    </div>
+                </Link>
+            ))}
+        </div>
     );
 };
 
